c9d06fe38f1605f72fa7146dfa217bcaad88c76f
Đỗ Tuấn Hưng <23021410@vnu.edu.vn>
2025-08-14 14:22:06 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ mongoose
 
 
 app.use(cors({
-  origin: ["https://blog-tech-sigma.vercel.app", "http://localhost:3000"],
+  origin: ["https://blog-tech-sigma.vercel.app", "http://localhost:3000", "https://gdgblogtech.hungproject.app"],
   methods: ["GET", "POST", 'PUT', 'DELETE'], 
   credentials: true 
 }));
